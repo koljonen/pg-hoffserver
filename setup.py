f4ba7a1c8780a59bbdb77c13156579622cd6261c
commitmachine <commitmachine@asdasd.se>
2017-02-16 16:04:50 +0100

    update version and dependency versions

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 install_requirements = [
   'Flask==0.11.1',
-  'pgcli==1.2.0',
-  'psycopg2==2.6.2',
+  'pgcli>=1.2.0',
+  'psycopg2>=2.6.2',
   'simplejson>=2.1'
 ]
 
 setup(name='pg-hoffserver',
-    version='0.1.4',
-    description='json interface to pgcli',
+    version='0.2.0',
+    description='json interface to postgres and pgcli',
     url='http://github.com/commitmachine/pg-hoffserver',
     author='commitmachine',
     author_email='commitmachine@asdasd.se',
